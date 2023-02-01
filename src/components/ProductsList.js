83a6aabad2efffcda8120febda3d4a9ab0e18824
KacperW57 <118220670+KacperW57@users.noreply.github.com>
2023-02-01 13:59:23 +0100

    added filtering by id functionality

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -27,22 +27,35 @@ const ArrowNext = styled.img`
   transform: rotate(0.25turn);
 `;
 
-export default function ProductsList() {
+export default function ProductsList(props) {
   const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState({});
   const [page, setPage] = useState("");
   const params = useParams();
   useEffect(() => {
     setPage(params.page);
   }, [params.page]);
-  console.log(params.page);
-  axios
-    .get(`https://reqres.in/api/products?per_page=5&page=${page}`)
-    .then((res) => {
-      setProducts(res.data.data);
-    })
-    .catch((err) => {
-      alert(err);
-    });
+
+  if (!props.filter) {
+    axios
+      .get(`https://reqres.in/api/products?per_page=5&page=${page}`)
+      .then((res) => {
+        setProducts(res.data.data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  } else if (props.filter) {
+    axios
+      .get(`https://reqres.in/api/products/${props.filter}`)
+      .then((res) => {
+        setProduct(res.data.data);
+      })
+      .catch((err) => {
+        props.removeFilter();
+        alert(`${err}. There are only 12 items in the list!`);
+      });
+  }
 
   const productsTable = products.map((product) => {
     return (
@@ -53,7 +66,77 @@ export default function ProductsList() {
       </Tr>
     );
   });
-  if (params.page === "1") {
+  if (!props.filter) {
+    if (params.page === "1") {
+      return (
+        <div>
+          <Table>
+            <thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Year</Th>
+              </Tr>
+            </thead>
+            <tbody>{productsTable}</tbody>
+          </Table>
+
+          <Link to={`/page/3`}>
+            <ArrowPrevious src={ArrowUp}></ArrowPrevious>
+          </Link>
+          <Link to={`/page/2`}>
+            <ArrowNext src={ArrowUp}></ArrowNext>
+          </Link>
+        </div>
+      );
+    } else if (params.page === "2") {
+      return (
+        <div>
+          <Table>
+            <thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Year</Th>
+              </Tr>
+            </thead>
+            <tbody>{productsTable}</tbody>
+          </Table>
+
+          <Link to={`/page/1`}>
+            <ArrowPrevious src={ArrowUp}></ArrowPrevious>
+          </Link>
+          <Link to={`/page/3`}>
+            <ArrowNext src={ArrowUp}></ArrowNext>
+          </Link>
+        </div>
+      );
+    } else if (params.page === "3") {
+      return (
+        <div>
+          <Table>
+            <thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Year</Th>
+              </Tr>
+            </thead>
+            <tbody>{productsTable}</tbody>
+          </Table>
+
+          <Link to={`/page/2`}>
+            <ArrowPrevious src={ArrowUp}></ArrowPrevious>
+          </Link>
+          <Link to={`/page/1`}>
+            <ArrowNext src={ArrowUp}></ArrowNext>
+          </Link>
+        </div>
+      );
+    } else if (params.page !== "1" || params.page !== 2 || params.page !== 3) {
+      return <h2>There are only 3 pages on the products list!</h2>;
+    }
+  } else if (props.filter) {
     return (
       <div>
         <Table>
@@ -64,62 +147,15 @@ export default function ProductsList() {
               <Th>Year</Th>
             </Tr>
           </thead>
-          <tbody>{productsTable}</tbody>
-        </Table>
-
-        <Link to={`/page/3`}>
-          <ArrowPrevious src={ArrowUp}></ArrowPrevious>
-        </Link>
-        <Link to={`/page/2`}>
-          <ArrowNext src={ArrowUp}></ArrowNext>
-        </Link>
-      </div>
-    );
-  } else if (params.page === "2") {
-    return (
-      <div>
-        <Table>
-          <thead>
-            <Tr>
-              <Th>ID</Th>
-              <Th>Name</Th>
-              <Th>Year</Th>
+          <tbody>
+            <Tr style={{ backgroundColor: `${product.color}` }}>
+              <Td>{product.id}</Td>
+              <Td>{product.name}</Td>
+              <Td>{product.year}</Td>
             </Tr>
-          </thead>
-          <tbody>{productsTable}</tbody>
+          </tbody>
         </Table>
-
-        <Link to={`/page/1`}>
-          <ArrowPrevious src={ArrowUp}></ArrowPrevious>
-        </Link>
-        <Link to={`/page/3`}>
-          <ArrowNext src={ArrowUp}></ArrowNext>
-        </Link>
       </div>
     );
-  } else if (params.page === "3") {
-    return (
-      <div>
-        <Table>
-          <thead>
-            <Tr>
-              <Th>ID</Th>
-              <Th>Name</Th>
-              <Th>Year</Th>
-            </Tr>
-          </thead>
-          <tbody>{productsTable}</tbody>
-        </Table>
-
-        <Link to={`/page/2`}>
-          <ArrowPrevious src={ArrowUp}></ArrowPrevious>
-        </Link>
-        <Link to={`/page/1`}>
-          <ArrowNext src={ArrowUp}></ArrowNext>
-        </Link>
-      </div>
-    );
-  } else if (params.page !== "1" || params.page !== 2 || params.page !== 3) {
-    return <h2>There are only 3 pages on the products list!</h2>;
   }
 }
